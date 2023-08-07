7ae756ddeaa03bb7e3ce5a595f72c418ff754070
Emre Ergül <emredevsalot@gmail.com>
2023-08-07 14:45:50 +0300

    add realignment and audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,66 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+const colors = [
+  "#FAF0CA",
+  "#F4D35E",
+  "#EE964B",
+  "#F95738",
+  "#EE964B",
+  "#F4D35E",
+  "#FAF0CA",
+  "#F4D35E",
+  "#EE964B",
+];
+
+const settings = {
+  tau: 2 * Math.PI,
+  maxLoops: Math.max(colors.length, 60), // Maximum loop amount the fastest element will make. (Must be above colors.length)
+  realignDuration: 600, // Total time for all dots to realign at the starting point
+  audioVolume: 0.2,
+};
+
+const playTestAudio = () => {
+  const audio = new Audio(`/key1.mp3`);
+  audio.volume = settings.audioVolume;
+  audio.play();
+};
+
+const calculateNextImpactTime = (
+  currentImpactTime: number,
+  velocity: number
+) => {
+  return currentImpactTime + Math.PI / velocity;
+};
+
+const arcs = colors.map((color, index) => {
+  const audio = new Audio(`/key${index + 1}.mp3`);
+  audio.volume = settings.audioVolume;
+
+  const numberOfLoops = settings.tau * (settings.maxLoops - index);
+  const velocity = numberOfLoops / settings.realignDuration;
+  const nextImpactTime = calculateNextImpactTime(0, velocity);
+  return {
+    color,
+    audio,
+    velocity,
+    nextImpactTime,
+  };
+});
+
 function App() {
   const [timerRunning, setTimerRunning] = useState(false);
+  const [soundEnabled, setSoundEnabled] = useState(true);
+  const toggleTimer = () => {
+    setTimerRunning(!timerRunning);
+  };
+
+  const toggleSound = () => {
+    setSoundEnabled(!soundEnabled);
+  };
+  useEffect(() => {
+    document.onvisibilitychange = () => setSoundEnabled(false);
+  }, []);
 
   const useAnimationFrame = (callback: any) => {
     // Use useRef for mutable variables that we want to persist
@@ -63,34 +121,58 @@ function App() {
     context.stroke();
     //#endregion
 
-    //#region Arc
-    const center = {
-      x: canvas.width * 0.5,
-      y: canvas.height * 0.9,
-    };
+    //#region Arcs & Circles
+    const lineLength = end.x - start.x;
+    const mostInnerArcRadius = lineLength * 0.05;
+    const spacing = (lineLength / 2 - mostInnerArcRadius) / arcs.length;
 
-    const length = end.x - start.x;
+    arcs.forEach((arc, index) => {
+      //#region Arc
+      const arcRadius = mostInnerArcRadius + index * spacing;
 
-    context.beginPath();
-    context.arc(center.x, center.y, length * 0.05, Math.PI, 2 * Math.PI);
-    context.stroke();
-    //#endregion
+      const center = {
+        x: canvas.width * 0.5,
+        y: canvas.height * 0.9,
+      };
 
-    //#region Circle
-    const arcRadius = length * 0.05;
-    const velocity = 1;
-    const maxAngle = 2 * Math.PI;
-    const distance = Math.PI + time * velocity;
-    const modDistance = distance % maxAngle;
-    const adjustedDistance =
-      modDistance >= Math.PI ? modDistance : maxAngle - modDistance;
+      context.beginPath();
+      context.strokeStyle = arc.color;
+      context.arc(center.x, center.y, arcRadius, Math.PI, 2 * Math.PI);
+      context.stroke();
+      //#endregion
 
-    const x = center.x + arcRadius * Math.cos(adjustedDistance);
-    const y = center.y + arcRadius * Math.sin(adjustedDistance);
+      //#region Circle
+      const distance = Math.PI + time * arc.velocity;
+      const modDistance = distance % settings.tau;
+      const adjustedDistance =
+        modDistance >= Math.PI ? modDistance : settings.tau - modDistance;
 
-    context.beginPath();
-    context.arc(x, y, length * 0.0065, 0, 2 * Math.PI);
-    context.fill();
+      const x = center.x + arcRadius * Math.cos(adjustedDistance);
+      const y = center.y + arcRadius * Math.sin(adjustedDistance);
+
+      context.beginPath();
+
+      context.fillStyle = arc.color;
+
+      context.arc(x, y, lineLength * 0.025, 0, 2 * Math.PI);
+      context.fill();
+
+      if (index === 0) {
+        console.log("Time: ", time.toFixed(2));
+        console.log("arc.nextImpactTime", arc.nextImpactTime);
+      }
+
+      if (time >= arc.nextImpactTime) {
+        if (soundEnabled) {
+          arc.audio.play();
+        }
+        arc.nextImpactTime = calculateNextImpactTime(
+          arc.nextImpactTime,
+          arc.velocity
+        );
+      }
+      //#endregion
+    });
     //#endregion
   };
 
@@ -115,9 +197,11 @@ function App() {
   return (
     <>
       <div className="absolute text-white flex gap-4">
-        <button onClick={() => setTimerRunning(!timerRunning)}>
-          {timerRunning ? "Stop" : "Start"}
+        <button onClick={toggleTimer}>{timerRunning ? "Stop" : "Start"}</button>
+        <button onClick={toggleSound}>
+          {soundEnabled ? "Mute" : "Unmute"}
         </button>
+        <button onClick={playTestAudio}>Test Audio</button>
         <Timer />
       </div>
       <Canvas />
